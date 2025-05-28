aa0e960e6c70db08620b03e4a8ed55ffc3ae70f2
Priy1000 <117532875+Priy1000@users.noreply.github.com>
2025-05-28 22:12:49 +0530

    part2

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,17 @@
 console.log("ciao");
+console.log(+true)
+console.log(+" ");
+//number+string=string 
+
+//string+number=string
+
+//if we'll write + behind a value then it converts value to a number(impooooortantt)
+
+console.log(1+2+(+"3")) // here the + before 3 and within the bracket acts as unary operator and tries to convert string to number
+
+let a=2;
+let b=3;
+console.log(`a:${a}, b:${b}`);
+console.log(`sum:${a+b}`)
+// to embed variables,calculations we can use string interpolation 
+
